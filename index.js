8be248c1f563453b115bcb11822e2a79653473b4
Ahmad Mamdouh ALhabrawi <mamdouh486@gmail.com>
2020-09-08 23:16:19 +0300

    change the input

--- index.js
+++ index.js
@@ -11,4 +11,15 @@ const Notes = require('./lib/notes.js');
 
 const options = new Input();
 
+const notes = new Notes();
+
+options.IsValid() ? notes.execute(options) : catchError();
+
+function catchError(){
+    console.log(`
+    use: -add or -a <your note>
+    -add or -a adding new note
+    `);
+}
+
 Notes.execute(options);
